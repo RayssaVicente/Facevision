cf310fc59549adb01dbac6e1cbbe73dbfd3155a9
LuanCarlosL <luancarloslaurentino52@gmail.com>
2025-02-21 20:39:12 -0300

    adicionado salvar dados e html corrigido

--- Facevisions/Facevision/views.py
+++ Facevisions/Facevision/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
+import os
+import json
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
 
 def home(request):
     return render(request, 'login/index.html')
 
 def cadastro(request):
     return render(request, 'cadastro/index.html')
+
+def salvar_dados(request):
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')  
+        curso = request.POST.get('curso')
+        dados_usuario = {
+            "nome": nome,
+            "email": email,
+            "senha": senha, 
+            "curso": curso
+        }
+        caminho_pasta = os.path.join(settings.BASE_DIR, "dados_usuarios")
+        caminho_arquivo = os.path.join(caminho_pasta, "dados.json")
+
+        try: #recuperar os dados
+            with open(caminho_arquivo, "r", encoding="utf-8") as arquivo:
+                dados_existentes = json.load(arquivo)
+        except (FileNotFoundError, json.JSONDecodeError):
+            dados_existentes = []
+        dados_existentes.append(dados_usuario)
+        with open(caminho_arquivo, "w", encoding="utf-8") as arquivo: #salvar os dados
+            json.dump(dados_existentes, arquivo, indent=4, ensure_ascii=False)
+    return HttpResponse("Usuário cadastrado com sucesso!", content_type="text/plain") 
+
+
+#basicamente ele não tá salvando os dados no json, so ta exibindo a mensagem
+#mais tá tudo feito pra dar cert já so falta melhorar essa função para dra certo
+
--- Facevisions/Facevisions/urls.py
+++ Facevisions/Facevisions/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from Facevision.views import home, cadastro
+from Facevision.views import home, cadastro, salvar_dados
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),  # Página inicial carregando o template HTML
-    path('cadastro/', cadastro, name='cadastro'),
+    path('cadastro/', cadastro, name='cadastro'), #carrega a pagina htm cadastro
+    path('salvar-dados/', salvar_dados, name='salvar_dados')# carrega a função de salvar dados, mais não está salvando 
 ]
