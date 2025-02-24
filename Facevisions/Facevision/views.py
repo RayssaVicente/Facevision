3564dd72052efc5c2e109ddfab61a4d691f72298
LuanCarlosL <luancarloslaurentino52@gmail.com>
2025-02-24 19:47:53 -0300

    login_criptografado

--- Facevisions/Facevision/views.py
+++ Facevisions/Facevision/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import os
 import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+import re
+from django.contrib.auth.hashers import make_password, check_password
+
 
 def home(request):
     return render(request, 'login/index.html')
@@ -17,10 +20,15 @@ def salvar_dados(request):
         email = request.POST.get('email')
         senha = request.POST.get('senha')  
         curso = request.POST.get('curso')
+        senha_valida = re.fullmatch(r'^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%#*?&]{8,}$', senha)
+        if not senha_valida:
+            return HttpResponse("A senha deve ter pelo menos 8 caracteres, uma letra maiuscula, um numero e um caractere especial.", content_type="text/plain")
+        senha_hash = make_password(senha)
+
         dados_usuario = {
             "nome": nome,
             "email": email,
-            "senha": senha, 
+            "senha": senha_hash, 
             "curso": curso
         }
         caminho_pasta = os.path.join(settings.BASE_DIR, "dados_usuarios")
@@ -34,7 +42,29 @@ def salvar_dados(request):
         dados_existentes.append(dados_usuario)
         with open(caminho_arquivo, "w", encoding="utf-8") as arquivo: #salvar os dados
             json.dump(dados_existentes, arquivo, indent=4, ensure_ascii=False)
-    return HttpResponse("Usuário cadastrado com sucesso!", content_type="text/plain") 
+    return HttpResponse("Usuario cadastrado com sucesso!", content_type="text/plain") 
+def login_view(request):
+    if request.method== "POST":
+
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        caminho_pasta = os.path.join(settings.BASE_DIR, "dados_usuarios")
+        caminho_arquivo = os.path.join(caminho_pasta, "dados.json")
+        try:
+            with open(caminho_arquivo, "r", encoding="utf-8") as arquivo:
+                dados_usuarios = json.load(arquivo)
+        except (FileNotFoundError, json.JSONDecodeError):
+            dados_usuarios = []
+        
+        for usuario in dados_usuarios:
+            if usuario["email"] == email:
+                if check_password(senha,usuario["senha"]):
+                    return HttpResponse("Login realizado com sucesso!", content_type="text/plain")
+                else:
+                    return HttpResponse("senha incorreta", content_type="text/plain")
+        else:
+            return HttpResponse("email nao encontrado!", content_type='text/plain')
+    return render(request, 'login/index.html')
 
 
 #basicamente ele não tá salvando os dados no json, so ta exibindo a mensagem
--- Facevisions/Facevisions/urls.py
+++ Facevisions/Facevisions/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from Facevision.views import home, cadastro, salvar_dados
+from Facevision.views import home, cadastro, salvar_dados, login_view
+
+
 
 
 
@@ -8,5 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),  # Página inicial carregando o template HTML
     path('cadastro/', cadastro, name='cadastro'), #carrega a pagina htm cadastro
-    path('salvar-dados/', salvar_dados, name='salvar_dados')# carrega a função de salvar dados, mais não está salvando 
+    path('salvar-dados/', salvar_dados, name='salvar_dados'),
+    path('login/', login_view, name='login'),# carrega a função de salvar dados, mais não está salvando 
 ]
