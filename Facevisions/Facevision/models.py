d8c291d17a7fda1d08d19ae1ce5f36cf53cb3b9f
Rayssa Vicente da Silva Viegas <102470604+RayssaVicente@users.noreply.github.com>
2025-12-21 18:41:37 -0300

    atualização de pastas e reformulação dos arquivos

--- Facevisions/Facevision/models.py
+++ Facevisions/Facevision/models.py
@@ -15,6 +15,20 @@ class Aluno(models.Model):
     def __str__(self):
         return self.nome
 
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Avaliacao(models.Model):
+    professor = models.ForeignKey(User, on_delete=models.CASCADE)
+    nome_aluno = models.CharField(max_length=100)
+    materia = models.CharField(max_length=50)
+    nota = models.DecimalField(max_digits=4, decimal_places=2)
+    data_lancamento = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nome_aluno} - {self.nota}"
+
     
 
 
